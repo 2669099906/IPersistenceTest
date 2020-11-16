cf52cd9cfcf0a95563955eb17f5e62554b5cf451
zhaoxiangrui <15546113287@163.com>
2020-11-16 21:00:00 +0800

    添加增删改查例子

--- src/main/java/com/zxr/test/IPersistenceTest.java
+++ src/main/java/com/zxr/test/IPersistenceTest.java
@@ -8,6 +8,7 @@ import com.zxr.sqlSession.SqlSession;
 import com.zxr.sqlSession.SqlSessionFactory;
 import com.zxr.sqlSession.SqlSessionFactoryBuilder;
 import org.dom4j.DocumentException;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.beans.PropertyVetoException;
@@ -19,50 +20,46 @@ import java.util.List;
  * @date 2020/11/8 20:03
  */
 public class IPersistenceTest {
+    UserMapper userMapper;
 
-    public static void main1(String[] args) throws Exception {
-        InputStream resourceAsStream = Resources.getResourceAsStream("./sqlMapConfig.xml");
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
-        SqlSession sqlSession = sqlSessionFactory.openSession();
-        User user = new User();
-        user.setId(1);
-        user.setUsername("张三");
-//        User userResult = (User)sqlSession.selectOne("user.selectOne", user);
-//        System.out.println(userResult.toString());
-//        List<User> objects = sqlSession.selectList("user.selectList");
-//        System.out.println(objects);
-//        UserDaoImpl userDao = new UserDaoImpl();
-//        List<User> all = userDao.findAll();
-//        System.out.println(all);
-//        User byCondition = userDao.findByCondition(user);
-//        System.out.println(byCondition);
-        IUserDao mapper = sqlSession.getMapper(IUserDao.class);
-        List<User> all = mapper.findAll();
-        User byCondition = mapper.findByCondition(user);
-        System.out.println(all);
-        System.out.println(byCondition);
-    }
-
-    @Test
-    public  void insertTest() throws PropertyVetoException, DocumentException {
+    @Before
+    public void before() throws PropertyVetoException, DocumentException {
         InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
         SqlSession sqlSession = sqlSessionFactory.openSession();
-        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
-        List<User> all = mapper.findAll();
-        System.out.println(all);
+        userMapper = sqlSession.getMapper(UserMapper.class);
+    }
+
+    @Test
+    public  void selectTest() {
+        List<User> allUser = userMapper.findAll();
+        allUser.forEach(System.out::println);
+    }
+
+    @Test
+    public void insertTest(){
         User user = new User();
-        user.setId(6);
-        user.setUsername("max");
-        user.setPassword("111111");
-        user.setBirthday("2020-11-23");
-//        Integer insert = mapper.insert(user);
-//        System.out.println(insert);
-        user.setPassword("1122");
-        Integer integer = mapper.updateById(user);
+        user.setUsername("mybatis");
+        user.setId(20);
+        Integer insert = userMapper.insert(user);
+        System.out.println(insert);
+    }
+
+    @Test
+    public void updateTest(){
+        User user = new User();
+        user.setId(20);
+        user.setUsername("MP");
+        Integer integer = userMapper.updateById(user);
         System.out.println(integer);
-        Integer integer1 = mapper.deleteById(user);
-        System.out.println(integer1);
+    }
+
+    @Test
+    public void deleteTest(){
+        User user = new User();
+        user.setId(20);
+        Integer integer = userMapper.deleteById(user);
+        System.out.println(integer);
     }
 
 }
